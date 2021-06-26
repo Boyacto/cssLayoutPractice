d33060cbdb87ec6ff77b4215aeda8cd4ded6c96d
Boyacto <bohyeoncha2000@gmail.com>
2021-06-26 23:49:52 +0900

    gulp project started scss project started

---
+++ gulpPractice/src/js/main.js
@@ -0,0 +1,6 @@
+impor { ramdom } from "./util";
+
+const rOne = random(10);
+const rOne = random(20);
+
+console.log(`${rOne} ${rTwo}`);
---
+++ gulpPractice/src/js/util.js
@@ -0,0 +1,1 @@
+export const ramdom = () => Math.floor(Math.random()*max);
